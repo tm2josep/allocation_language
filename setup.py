f55fa78d05909ca0767aee6fd4c162fef2d579c9
tm2josep <tmammenjoseph@gmail.com>
2022-01-22 00:40:32 -0500

    init files added

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name="allocation_language",
     version="0.0.1",
     description="Language intented to construct the financial behaviour of (re)insurance contracts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=[
         "allocation_language",
         "alloc_lang_data_containers/csv_to_event_list",
         "alloc_lang_data_containers/converters",
-        ],
+    ],
     install_requires=[
         "rply"
     ],
