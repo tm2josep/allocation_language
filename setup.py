eb55cc4c418f0435f5145c9cc0848b6a885d9ec6
tm2josep <tmammenjoseph@gmail.com>
2022-03-16 15:50:26 -0400

    making aggregation node more readable

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+    
 setuptools.setup(
     name="allocation_language",
     version="0.0.4",
--- src/allocation_language/ast_nodes/alloc_lang_aggregate.py
+++ src/allocation_language/ast_nodes/alloc_lang_aggregate.py
@@ -1,13 +1,13 @@
+from multiprocessing import Event
 from typing import Iterable
 from allocation_language.alloc_lang_data_containers.event_dataclasses import AssessmentEvent, EventData
 from allocation_language.ast_nodes.alloc_lang_primitives import AggField, Field, Node
 
-class KeylessAggregateNode(Node):
+class AggregateNode(Node):
     def __init__(self, agg_field_nodes: Iterable[AggField]):
         self.agg_field_nodes = agg_field_nodes
-
+    
     def evaluate_stream(self, events: Iterable[EventData]) -> Iterable[EventData]:
-        events = list(events)
         remaining_events = []
         for event in events:
             if not isinstance(event, EventData):
@@ -17,50 +17,28 @@ class KeylessAggregateNode(Node):
             if not event.scope_flag:
                 yield event
                 continue
-            
-            remaining_events.append(event)
 
+            remaining_events.append(event)
+        yield from self.aggregate_stream(remaining_events)
+    
+    def aggregate_stream(self, events: Iterable[EventData]):
         agg_data = {}
         for agg_node in self.agg_field_nodes:
-            agg_data[agg_node.name] = agg_node.evaluate_stream(remaining_events)
-
+            agg_data[agg_node.name] = agg_node.evaluate_stream(events)
+        
         yield EventData(data=agg_data, scope_flag=True, aggregated=True)
 
-    def evaluate(self, event: EventData) -> AssessmentEvent:
-        if not event.scope_flag:
-            return event
-        return AssessmentEvent(event.data[self.agg_field_node.name])
+class KeylessAggregateNode(AggregateNode):
+    def __init__(self, agg_field_nodes: Iterable[AggField]):
+        super().__init__(agg_field_nodes)
 
 
-class KeyedAggregateNode(Node):
-    def __init__(self, key: Field, agg_field_nodes: Iterable[AggField]):
-        self.key = key
-        self.agg_field_nodes = agg_field_nodes
+class KeyedAggregateNode(AggregateNode):
+    def __init__(self, key_field: Field, agg_field_nodes: Iterable[AggField]):
+        self.key_field = key_field
+        super().__init__(agg_field_nodes)
 
-    def evaluate_stream(self, events: Iterable[EventData]) -> Iterable[EventData]:
-        events = list(events)
-        temp_events = []
-        for event in events:
-            if not isinstance(event, EventData):
-                yield event
-            else:
-                temp_events.append(event)
-
-        events = temp_events
-        for current_key in set(
-            self.key.evaluate_stream(event for event in events if event.scope_flag)
-        ):
-            agg_data = {}
-            for agg_node in self.agg_field_nodes:
-                agg_data[agg_node.name] = agg_node.evaluate_stream(
-                    event for event in events if event.data[self.key.name] == current_key
-                )
-            agg_data[self.key.name] = current_key
-            yield EventData(data=agg_data, scope_flag=True, aggregated=True)
-            
-        yield from [event for event in events if not event.scope_flag]
-
-    def evaluate(self, event: EventData) -> AssessmentEvent:
-        if not event.scope_flag:
-            return event
-        return AssessmentEvent(event.data[self.agg_field_node.name])
+    def aggregate_stream(self, events: Iterable[EventData]):
+        keys = set(self.key_field.evaluate_stream(filter(lambda x: x.scope_flag, events)))
+        for key in keys:
+            yield from super().aggregate_stream(filter(lambda x: x.data[self.key.name] == key))
